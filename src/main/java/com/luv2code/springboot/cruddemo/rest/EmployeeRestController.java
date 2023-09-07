8e5d91641c53f29e721ab50ebe33ec3f341bb127
Pranay Harwani <pranayiitian1@gmail.com>
2023-09-07 20:35:04 +0530

    GetReq
    By Id

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,13 @@ public EmployeeRestController(EmployeeService theEmployeeService){
     public List<Employee> findAll(){
     return employeeService.findAll();
 
+}
+@GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+    Employee theEmployee=employeeService.findById(employeeId);
+    if(theEmployee==null){
+        throw new RuntimeException("Employee id not find-"+employeeId);
+    }
+    return theEmployee;
 }
 }
