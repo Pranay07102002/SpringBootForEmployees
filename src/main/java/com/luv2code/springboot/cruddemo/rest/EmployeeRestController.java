3ed79deaf5d8106783ac46a9993e69ce17e6d951
Pranay Harwani <pranayiitian1@gmail.com>
2023-09-07 20:44:51 +0530

    Post Request

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,10 +3,7 @@ package com.luv2code.springboot.cruddemo.rest;
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,4 +30,13 @@ public EmployeeRestController(EmployeeService theEmployeeService){
     }
     return theEmployee;
 }
+
+@PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+    theEmployee.setId(0);
+    Employee dbEmployee=employeeService.save(theEmployee);
+
+    return dbEmployee;
+}
+
 }
